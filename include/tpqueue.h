44a33da4bf7861ce1a6f57a1f746f4ef70287346
Acsolobalt <78843052+Acsolobalt@users.noreply.github.com>
2021-05-07 19:29:36 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,7 +5,60 @@
 
 template<typename T>
 class TPQueue {
-  // Сюда помещается описание структуры "Очередь с приоритетами"
+  struct ITEM {
+    T data;
+		ITEM * next;
+	};
+private:
+  TPQueue::ITEM * create(T data) {
+    ITEM * item = new ITEM;
+		item->data = data;
+		item->next = nullptr;
+		return item;
+	};
+  ITEM * head;
+	ITEM * tail;
+public:
+	TPQueue() : head(nullptr), tail(nullptr) {}
+	~TPQueue()
+	{
+		while (head)
+			pop();
+	}
+	void push(const T & data)
+	{
+		if (tail && head) {
+			ITEM * item = create(data);
+			ITEM * temp = head;
+			while (temp) {
+				if (temp->next == nullptr) {
+					tail->next = item;
+					tail = tail->next;
+				}
+				else {
+					if (item->data.prior > temp->next->data.prior) {
+						item->next = temp->next;
+						temp->next = item;
+            break;
+					}
+				}
+			}
+		}
+		else {
+			head = create(data);
+			tail = head;
+		}
+	}
+
+	T pop()
+	{
+		assert(head);
+		ITEM* tmp = head->next;
+		T data = head->data;
+		delete head;
+		head = tmp;
+		return data;
+	}
 };
 
 struct SYM {
